2257d8cfc23007153b19f6c387ac7a654dbde46f
山木隆寛 <yamaki.takahiro@gmail.com>
2019-01-11 20:11:29 +0800

    Day11 Moved business information to the container component (App) Passed information from parent components (App) to child components (BusinessList, Business)

--- js-react/ravenous/src/components/Business/Business.js
+++ js-react/ravenous/src/components/Business/Business.js
@@ -1,37 +1,24 @@
 import React from 'react';
 import './Business.css';
-// import ReactDOM from 'react-dom';
-
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
 class Business extends React.Component {
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+          <img src={this.props.business.imageSrc} alt=''/>
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.state} {this.props.business.zipCode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>ITALIAN</h3>
-            <h3 className="rating">4.5 stars}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating} stars</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
