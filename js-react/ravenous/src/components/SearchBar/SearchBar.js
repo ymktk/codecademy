3709e32ed107073bd499f917f394bef82d61584b
山木隆寛 <yamaki.takahiro@gmail.com>
2019-01-13 23:23:13 +0800

    Fix an error

--- js-react/ravenous/src/components/SearchBar/SearchBar.js
+++ js-react/ravenous/src/components/SearchBar/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}> {sortByOption} </li>;
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
     })
   }
 
@@ -20,7 +20,7 @@ class SearchBar extends React.Component {
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
           <ul>
-            {this.renderSortByOptions}
+            {this.renderSortByOptions()}
           </ul>
         </div>
         <div className="SearchBar-fields">
