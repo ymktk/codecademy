df84fe5b23afbde847c92441f6c1c5c55cb5251f
山木隆寛 <yamaki.takahiro@gmail.com>
2019-01-13 23:23:25 +0800

    Formatting

--- js-react/ravenous/src/components/BusinessList/BusinessList.js
+++ js-react/ravenous/src/components/BusinessList/BusinessList.js
@@ -7,7 +7,9 @@ class BusinessList extends React.Component {
       return (
         <div className="BusinessList">
           {
-            this.props.businesses.map(business => <Business business={business} />)
+            this.props.businesses.map(business => {
+              return <Business business={business} />
+            })
           }
         </div>
       );
